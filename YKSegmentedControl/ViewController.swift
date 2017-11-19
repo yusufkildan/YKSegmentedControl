1b45bb8888a30f9780b4f7091fc848528e30ecc6
yusufkildan <kildanyusuf@gmail.com>
2017-11-19 22:39:44 +0100

    Initial Commit

---
+++ YKSegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YKSegmentedControl
+//
+//  Created by yusuf_kildan on 19/11/2017.
+//  Copyright © 2017 yusuf_kildan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
